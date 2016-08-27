91afc5d9872206f598fac761c715c2645ebb1f3a
lukasmartinelli <me@lukasmartinelli.ch>
2016-08-27 20:41:25 +0200

    Order by area when dumping

--- src/export-shapefile/export-shapefile.sh
+++ src/export-shapefile/export-shapefile.sh
@@ -5,7 +5,7 @@ set -o nounset
 
 function export_shp() {
     local lake_shapefile="/data/osm_lake_polygon.shp"
-    local query="SELECT osm_id, ST_SimplifyPreserveTopology(geometry, 100) AS geometry FROM osm_lake_polygon WHERE area > 2 * 1000 * 1000 AND ST_GeometryType(geometry)='ST_Polygon' AND name <> ''"
+    local query="SELECT osm_id, ST_SimplifyPreserveTopology(geometry, 100) AS geometry FROM osm_lake_polygon WHERE area > 2 * 1000 * 1000 AND ST_GeometryType(geometry)='ST_Polygon' AND name <> '' ORDER BY area DESC"
     pgsql2shp -f "$lake_shapefile" \
         -h "$POSTGRES_PORT_5432_TCP_ADDR" \
         -u "$POSTGRES_ENV_POSTGRES_USER" \
