f07cd2203a26321a9f9d619595f87abbec184959
lukasmartinelli <me@lukasmartinelli.ch>
2016-08-27 14:35:45 +0200

    Calculate center lines

--- src/export-shapefile/export-shapefile.sh
+++ src/export-shapefile/export-shapefile.sh
@@ -5,7 +5,7 @@ set -o nounset
 
 function export_shp() {
 	local lake_shapefile="/data/osm_lake_polygon.shp"
-    local query="SELECT osm_id, ST_MakeValid(ST_Simplify(geometry, 100)) AS geometry FROM osm_lake_polygon WHERE area > 40000"
+    local query="SELECT osm_id, ST_MakeValid(ST_Simplify(geometry, 100)) AS geometry FROM osm_lake_polygon WHERE area > 2 * 1000 * 1000"
 	pgsql2shp -f "$lake_shapefile" \
         -h "$POSTGRES_PORT_5432_TCP_ADDR" \
         -u "$POSTGRES_ENV_POSTGRES_USER" \
