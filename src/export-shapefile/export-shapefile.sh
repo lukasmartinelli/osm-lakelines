f0d1dc3e87a54c71b121ea10c81cb5b34025f2ff
lukasmartinelli <me@lukasmartinelli.ch>
2016-08-27 16:55:03 +0200

    Ensure valid geoms

--- src/export-shapefile/export-shapefile.sh
+++ src/export-shapefile/export-shapefile.sh
@@ -4,9 +4,9 @@ set -o pipefail
 set -o nounset
 
 function export_shp() {
-	local lake_shapefile="/data/osm_lake_polygon.shp"
-    local query="SELECT osm_id, ST_MakeValid(ST_Simplify(geometry, 100)) AS geometry FROM osm_lake_polygon WHERE area > 2 * 1000 * 1000"
-	pgsql2shp -f "$lake_shapefile" \
+    local lake_shapefile="/data/osm_lake_polygon.shp"
+    local query="SELECT osm_id, ST_SimplifyPreserveTopology(geometry, 100) AS geometry FROM osm_lake_polygon WHERE area > 2 * 1000 * 1000 AND ST_GeometryType(geometry)='ST_Polygon'"
+    pgsql2shp -f "$lake_shapefile" \
         -h "$POSTGRES_PORT_5432_TCP_ADDR" \
         -u "$POSTGRES_ENV_POSTGRES_USER" \
         -P "$POSTGRES_ENV_POSTGRES_PASSWORD" \
